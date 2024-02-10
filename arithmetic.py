7328366ee0a81d542bd82a7f73d595fc9778a373
d4rcel <daraola05@gmail.com>
2024-02-10 21:36:00 +0100

    First commit

---
+++ arithmetic.py
@@ -0,0 +1,2 @@
+def arithmetic():
+    print("Arithmetic")
---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+
+from arithmetic import arithmetic
+
+arithmetic()
+
+# Demanderpyt à l'utilisateur son nom
+nom_utilisateur = input("Entrez votre nom : ")
+
+# Afficher un message de salutation
+print("Bonjour, " + nom_utilisateur + "! Bienvenue dans votre projet console en Python.")
